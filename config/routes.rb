4347f7d00e62d4de3bc3e8eb4b961036dc86429a
Woojin Han <verecahill.@gmail.com>
2012-07-19 18:42:12 +0900

    Finshed sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 HerokuTest::Application.routes.draw do
   resources :users
-  get "users/new"
+  resources :sessions, only: [:new, :create, :destroy]
   
-  match '/signup', to: 'users#new'
-
+  match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/help', to: 'static_pages#help'
 
   match '/about', to: 'static_pages#about'
